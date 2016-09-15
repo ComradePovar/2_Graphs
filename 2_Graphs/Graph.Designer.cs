76221787b295fbef596af64223e53b70e723442a
Maxim <Maxim@TOKAREVMAXIM-PC>
2016-09-15 22:01:12 +0300

    Did some research. Величина погрешности увеличивается к середине отрезка разбиения(между двумя узлами интерполяции). Наименьшая величина погрешности в середине отрезка разбиения находится между двумя центральными узлами интерполяции.

--- 2_Graphs/Graph.Designer.cs
+++ 2_Graphs/Graph.Designer.cs
@@ -35,6 +35,7 @@
             this.lbM = new System.Windows.Forms.Label();
             this.lbN = new System.Windows.Forms.Label();
             this.tbN = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.grpBox.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -53,6 +54,7 @@
             // 
             // grpBox
             // 
+            this.grpBox.Controls.Add(this.button1);
             this.grpBox.Controls.Add(this.btnDraw);
             this.grpBox.Controls.Add(this.tbM);
             this.grpBox.Controls.Add(this.lbM);
@@ -108,6 +110,16 @@
             this.tbN.TabIndex = 0;
             this.tbN.Tag = "N";
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(557, 20);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // Graph
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -134,6 +146,7 @@
         private System.Windows.Forms.Button btnDraw;
         private System.Windows.Forms.TextBox tbM;
         private System.Windows.Forms.Label lbM;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- 2_Graphs/Graph.cs
+++ 2_Graphs/Graph.cs
@@ -81,17 +81,70 @@ namespace _2_Graphs
                 coloredPoints.Points.Add(new ScatterPoint(interPoints[i], f(interPoints[i])));
             }
             polynomial.InterPoints = interPoints;
-            model.Series.Add(coloredPoints);
+            //model.Series.Add(coloredPoints);
         }
+        double[] errors;
         private void PlotGraphs(double segmentLength, int M)
         {
+            double k = segmentLength / M;
+            errors = new double[(int)(2/k)];
             model.Series.Add(new FunctionSeries(f, polynomial.LowerFunctionBound,
                 polynomial.UpperFunctionBound, segmentLength / M, $"ln(1 + x^2)/(1+x^2) c параметром M={M}"));
+
+            for (int i = 0; i < errors.Length; i++)
+            {
+                errors[i] = f(i * k);
+            }
             model.Series.Add(new FunctionSeries(polynomial.GetPolynomialValue, polynomial.LowerFunctionBound,
                 polynomial.UpperFunctionBound, segmentLength / M, $"Многочлен Лагранжа степени {polynomial.Degree}"));
-        
+            for(int i = 0; i < errors.Length; i++)
+            {
+                errors[i] = Math.Abs(errors[i] - polynomial.GetPolynomialValue(i*k));
+            }
+            string[] str_errors = new string[errors.Length];
+            for (int i = 0; i < errors.Length; i++)
+            {
+                str_errors[i] = errors[i].ToString();
+            }
+            System.IO.File.WriteAllLines(@"H:\Projects\C#\Lab1\2_Graphs\2_Graphs\TextFile1.txt", str_errors);
             plot.Model = model;
             plot.InvalidatePlot(true);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int M, segmentCount;
+            model.Series.Clear();
+            try
+            {
+                segmentCount = GetValue(tbN);
+                M = GetValue(tbM);
+            }
+            catch (ArgumentException)
+            {
+                return;
+            }
+
+            double segmentLength = (polynomial.UpperFunctionBound - polynomial.LowerFunctionBound) / segmentCount;
+            SetInterpolationPoints(segmentLength, segmentCount);
+            double k = segmentLength / M;
+            errors = new double[(int)(2 / k)];
+            for (int i = 0; i < errors.Length; i++)
+            {
+                errors[i] = f(i * k);
+            }
+            for (int i = 0; i < errors.Length; i++)
+            {
+                errors[i] = Math.Abs(errors[i] - polynomial.GetPolynomialValue(i * k));
+            }
+
+            model.Series.Add(new FunctionSeries(err, 0, errors.Length - 1, errors.Length, "errors"));
+            plot.Model = model;
+            plot.InvalidatePlot(true);
+        }
+        private double err(double index)
+        {
+            return errors[(int)index];
         }
     }
 }
